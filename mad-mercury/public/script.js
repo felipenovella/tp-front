ef5100b2bc487828093143d07715777013f65545
felipenovella <49439231@est.ort.edu.ar>
2026-03-27 14:18:06 -0300

    no funciona nada

--- mad-mercury/public/script.js
+++ mad-mercury/public/script.js
@@ -1,17 +1,28 @@
-const search = document.getElementById("search");
-const cards = document.querySelectorAll(".card");
+document.addEventListener("DOMContentLoaded", () => {
 
-search.addEventListener("input", () => {
-  const value = search.value.toLowerCase();
-
-  cards.forEach(card => {
-    const text = card.textContent.toLowerCase();
-    card.style.display = text.includes(value) ? "block" : "none";
+    const search = document.getElementById("search");
+    const planes = document.getElementById("planes");
+  
+    console.log("JS funcionando");
+  
+    search.addEventListener("input", async () => {
+      console.log("escribiendo...");
+  
+      const value = search.value;
+  
+      const res = await fetch("https://restcountries.com/v3.1/all");
+      const data = await res.json();
+  
+      const random = data[Math.floor(Math.random() * data.length)];
+  
+      planes.innerHTML = `
+        <div class="card">
+          <h2>${value || "Vuelo desconocido"}</h2>
+          <p>Origen: ${random.name.common}</p>
+          <p>Destino: ${data[0].name.common}</p>
+          <p>Estado: En vuelo ✈️</p>
+        </div>
+      `;
+    });
+  
   });
-});
-
-const btn = document.getElementById("darkBtn");
-
-btn.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-});
